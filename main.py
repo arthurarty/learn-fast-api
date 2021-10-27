7a1a72f279dd44626726bad16a973cdae8a843d5
arthurarty <arthur.nangai@gmail.com>
2021-10-27 17:25:46 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from datetime import date
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/date/{date_str}")
+def read_date(date_str: date):
+    return {
+        "date_obj": date
+    }
