e4a63b879d8ac76033c665f4cdb51db46a4f2c67
Arthur Nangai <arthur.nangai@gmail.com>
2021-10-28 00:18:53 +0300

    test out overwriting error

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from datetime import date
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from starlette.requests import Request
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    path = request.url.path
+    if path == '/items':
+        return PlainTextResponse(str(exc), status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items")
+def read_items(q: Optional[int] = None):
+    return {"message": "So you want to read everything haha"}
 
 
 @app.get("/items/{item_id}")
